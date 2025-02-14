8bf1f4f4e0c644e6ae81b06f0696056d2cc94365
Scoatty <scottosmundson@gmail.com>
2025-02-14 17:48:32 -0600

    adding export const prender = true to page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { default as Card, CardData } from '$lib/Card.svelte';
 	import Cropper from "svelte-easy-crop";
 	import { getCroppedImg } from '$lib/canvasUtils.js';
+	export const prerender = true;
 
 	let data = new CardData();
 
