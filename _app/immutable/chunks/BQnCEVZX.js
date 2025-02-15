3ba1d632249fa9e348830d3fcaba26627d29c1de
Scoatty <73728368+Scoatty@users.noreply.github.com>
2025-02-15 00:50:54 -0600

    Add files via upload

---
+++ _app/immutable/chunks/BQnCEVZX.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1lepeph)==null?void 0:s.base)??"/Scoatty.github.io";var e;const a=((e=globalThis.__sveltekit_1lepeph)==null?void 0:e.assets)??t;export{a,t as b};
