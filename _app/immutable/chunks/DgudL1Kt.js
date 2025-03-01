c965588cbb552cc993060833c3335c571049f9d8
Scoatty <73728368+Scoatty@users.noreply.github.com>
2025-02-28 23:03:45 -0600

    new bluetemplate and effect box rework

---
+++ _app/immutable/chunks/DgudL1Kt.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_mv9whc)==null?void 0:s.base)??"/Scoatty.github.io";var t;const e=((t=globalThis.__sveltekit_mv9whc)==null?void 0:t.assets)??a;export{e as a,a as b};
